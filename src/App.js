4fe79ee11dbb2f3817305647b0b1323ac4bc714e
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-09-26 17:17:44 -0700

    troubleshot route, link, and swtich methods. IN current state, list of books is printed to screen and clicking a book element will hcange the id in the windows search bar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path="/" component={BookList} />
+        <Route path="/books/:id" component={BookDetail} />
+      </Switch>
     </main>
   );
 }
--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,8 +1,11 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
+import { useParams, Link } from 'react-router-dom';
+
 
 function BookDetail() {
-  const id = 1; // TODO: Use id from route
+  const { id } = useParams();
+  // const id = 1; // TODO: Use id from route
   const { book, loading, error } = useBookDetail(id);
 
   if (error)
@@ -14,7 +17,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Link to='/books'>Back</Link>
+      <Book book={book} showDetail />;
+    </>
+  );
 }
 
 export default BookDetail;
